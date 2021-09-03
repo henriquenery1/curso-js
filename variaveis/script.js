1ee6670dcbe07ec033d01b10afaaae75e872d21f
Henrique Nery <88451907+henriquenery1@users.noreply.github.com>
2021-09-03 20:58:22 -0300

    Add files via upload

---
+++ variaveis/script.js
@@ -0,0 +1,27 @@
+/*
+Exercício:
+Dica rápida, frases (string) devem ser colocadas entre aspas
+(simples ou duplas) e números não levam aspas.
+*/
+
+// Declarar uma variável com o seu nome
+var nome = 'Henrique';
+
+// Declarar uma variável com a sua idade
+var idade = 22;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'pizza';
+
+// Declarar 5 variáveis diferentes sem valores
+var time,
+    professor, 
+    pais,
+    endereço,      
+    rua;
+
+console.log(nome, idade, comidaFavorita);
