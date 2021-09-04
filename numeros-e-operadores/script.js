40b6e4871c95b16c63f39677e235f6750a26d106
Henrique Nery <88451907+henriquenery1@users.noreply.github.com>
2021-09-03 21:00:07 -0300

    Add files via upload

---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,22 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+
+// Crie duas expressões que retornem NaN
+var ex1 = 'Teste' / 2;
+var ex2 = 'Teste' - 2;
+console.log(ex1, ex2);
+
+// Somar a string '200' com o número 50 e retornar 250
+var soma = +'200' + 50;
+console.log(soma);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var x = 5;
+console.log(++x);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+
+console.log(peso);
