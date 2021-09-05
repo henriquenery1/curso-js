3eb3623b5ba9f176481d16f8704134b1e114c7cd
Henrique Nery <88451907+henriquenery1@users.noreply.github.com>
2021-09-05 01:36:58 -0300

    Add files via upload

---
+++ dom-iniciantes/script.js
@@ -0,0 +1,16 @@
+//Exercício
+// Retorne o url da página atual utilizando o objeto window
+const url = window.location.href;
+console.log(url)
+
+// Seleciona o primeiro elemento da página que possua a classe ativo
+const eleAtivo = document.querySelector('.ativo')
+console.log(eleAtivo);
+
+// Retorne a linguagem do navegador
+const lang = window.navigator.language;
+console.log(lang);
+
+// Retorne a largura da janela
+const wid = window.innerWidth;
+console.log(wid);
